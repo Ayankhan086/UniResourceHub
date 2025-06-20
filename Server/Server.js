2e864719a7ce6eac8938d81bb917485e8360d565
Ayankhan086 <kmayankhan1@gmail.com>
2025-06-20 15:45:00 +0500

    fix: update CORS origin to allow requests from the production frontend URL

--- Server/Server.js
+++ Server/Server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-   origin: (origin, callback) => callback(null, true), // Change to your frontend URL for production
+   origin: ["https://uni-resource-hub.vercel.app"], // Change to your frontend URL for production
    credentials: true,
    allowedHeaders: ['Content-Type', 'Authorization']
 }));
