c6a6c34a2e418c5d951ff202fe7db437bccccff5
Ayankhan086 <kmayankhan1@gmail.com>
2025-06-20 13:50:54 +0500

    fix: remove unused path import and frontend serving logic for production

--- Server/Server.js
+++ Server/Server.js
@@ -8,7 +8,6 @@ import resourceRoutes from "./routes/Resource.routes.js"
 import activityRoutes from "./routes/Activity.routes.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"
-import path from "path"
 
 const app = express()
 
@@ -23,13 +22,6 @@ app.use(cors(
     }
 ))
 
-// Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../Client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../Client/dist', 'index.html'));
-  });
-}
 
 app.get("/",(req, res)=>{
     res.send("Okay")
