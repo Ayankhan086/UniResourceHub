fe6b7ec2c55557efb6defe39021bc975cb743c63
Ayankhan086 <kmayankhan1@gmail.com>
2025-06-20 14:18:57 +0500

    fix: refactor Server.js for consistency and add vercel configuration file

--- Server/Server.js
+++ Server/Server.js
@@ -7,36 +7,31 @@ import courseRoutes from "./routes/Courses.routes.js"
 import resourceRoutes from "./routes/Resource.routes.js"
 import activityRoutes from "./routes/Activity.routes.js"
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(
-    {
-       origin: (origin, callback) => callback(null, true), // Change to your frontend URL
-       credentials: true,
-       allowedHeaders: ['Content-Type', 'Authorization']
-    }
-))
+app.use(cors({
+   origin: (origin, callback) => callback(null, true), // Change to your frontend URL for production
+   credentials: true,
+   allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
+app.get("/", (req, res) => {
+    res.send("Okay");
+});
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/departments", deptRoutes);
+app.use("/api/v1/semesters", semRoutes);
+app.use("/api/v1/courses", courseRoutes);
+app.use("/api/v1/resources", resourceRoutes);
+app.use("/api/v1/activities", activityRoutes);
 
-app.get("/",(req, res)=>{
-    res.send("Okay")
-})
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/departments", deptRoutes)
-app.use("/api/v1/semesters", semRoutes)
-app.use("/api/v1/courses", courseRoutes)
-app.use("/api/v1/resources", resourceRoutes)
-app.use("/api/v1/activities", activityRoutes)
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`);
-})
-
-
+});
