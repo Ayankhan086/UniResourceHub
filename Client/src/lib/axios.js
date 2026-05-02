1d214b7874aced79726a582e6ac74febff999aa4
Ayankhan086 <kmayankhan1@gmail.com>
2026-05-02 08:53:33 +0500

    feat: create configured axios instance with base URL and authorization header

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import cookie from "js-cookie"
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1', // Change to your API base URL
+    baseURL: 'https://uni-resource-hub-18s1.vercel.app/api/v1', // Change to your API base URL
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
         'Authorization': `Bearer ${cookie.get('accessToken')}`
     },
     withCredentials: true, // Include credentials for CORS requests
